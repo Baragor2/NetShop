d947064d7b68284e7305d97ba3bcaf93051e82ac
Yauheni Mukhin <145256155+Baragor2@users.noreply.github.com>
2024-07-18 16:04:16 +0300

    create product

--- app/products/router.py
+++ app/products/router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from pydantic import PositiveInt
 
 from app.products.dao import ProductsDAO
-from app.products.schemas import SProductWithCategory
+from app.products.schemas import SProductWithCategory, SProduct
 
 router = APIRouter(
     prefix="/products",
@@ -10,7 +10,7 @@ router = APIRouter(
 )
 
 
-@router.get("")
+@router.get("/")
 async def get_products() -> list[SProductWithCategory]:
     products_with_categories = await ProductsDAO.get_products_with_categories()
     return products_with_categories
@@ -20,3 +20,9 @@ async def get_products() -> list[SProductWithCategory]:
 async def get_product(product_id: PositiveInt) -> SProductWithCategory:
     product_with_category = await ProductsDAO.get_product_with_category(product_id)
     return product_with_category
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_product(product: SProduct) -> dict[str, str]:
+    await ProductsDAO.add(**dict(product))
+    return {"message": "Product created"}
